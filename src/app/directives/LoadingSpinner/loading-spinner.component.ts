1ca09b9ba68d830a4af98b3e4d7c131aab199d25
Tal Gvili <gviligvili@gmail.com>
2017-06-18 20:27:27 +0300

    new Spinner

--- src/app/directives/LoadingSpinner/loading-spinner.component.ts
+++ src/app/directives/LoadingSpinner/loading-spinner.component.ts
@@ -6,7 +6,8 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'loading-spinner',
   template: `
-<div class='always-showing-loading-spinner-overlay'>
+<div style="height: 100%; width: 100%; position: relative">
+<div class='loading-spinner-overlay always-showing-loading-spinner-overlay'>
   <div class='loading-spinner-anim'>
     <div class='loading-spinner-border loading-spinner-out'></div>
     <div class='loading-spinner-border loading-spinner-in'></div>
@@ -26,6 +27,7 @@ import {Component} from '@angular/core';
       <span class='loading-spinner-dot'></span>
     </div>
   </div>
+</div>
 </div>
 `,
   styles: [''] // The Styles are in the src/styles.css file ! , because we need themfbefore compilation !
